2878415da31f4dd2ec71c8fb4d3c5059d10cbe0d
Thomas Abplanalp <info@tasoft.ch>
2019-10-20 13:07:08 +0200

    Initial Commit

--- src/AbstractPDO.php
+++ src/AbstractPDO.php
@@ -58,6 +58,29 @@ abstract class AbstractPDO extends PDO
         return $sql;
     }
 
+    public function query($statement, $mode = \PDO::ATTR_DEFAULT_FETCH_MODE, $arg3 = NULL, array $ctorargs = array())
+    {
+        if(is_string($statement))
+            $statement = $this->resolveSQLTablePrefix($statement);
+
+        return parent::query($statement, $mode, $arg3, $ctorargs);
+    }
+
+    public function exec($statement)
+    {
+        if(is_string($statement))
+            $statement = $this->resolveSQLTablePrefix($statement);
+        parent::exec($statement);
+    }
+
+    public function prepare($statement, array $driver_options = array())
+    {
+        if(is_string($statement))
+            $statement = $this->resolveSQLTablePrefix($statement);
+        return parent::prepare($statement, $driver_options);
+    }
+
+
     public function select(string $sql, array $arguments = [])
     {
         $sql = $this->resolveSQLTablePrefix($sql);
